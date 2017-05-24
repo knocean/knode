1b509398415597ba065e12d047fe1b9db4714335
inaimathi <leo.zovic@gmail.com>
2017-05-24 09:14:26 -0400

    Working FTP redirects for upstream ontologies
    - fetch-upstream now appropriately follows FTP redirects instead of
      throwing a nullpointer error

--- src/knode/upstream.clj
+++ src/knode/upstream.clj
@@ -5,15 +5,30 @@
    [org.httpkit.client :as http]
    [clojure.data.xml :as xml]))
 
-;; (xml/parse (java.io.StringReader. (:body @(http/get "https://raw.githubusercontent.com/IEDB/MRO/v2016-12-15/mro.owl"))))
-
-(defn xml-string->version-iri [string]
-  (->> string
-       java.io.StringReader. xml/parse
+(defn xml->version-iri [stream]
+  (->> stream
+       xml/parse
        :content first :content (filter #(= :versionIRI (:tag %)))
        first :attrs :rdf/resource))
 
-(defn spit-gzipped! [path content]
+(defn xml-string->version-iri [string]
+  (xml->version-iri (java.io.StringReader. string)))
+
+(defmulti spit-gzipped! #(class %2))
+
+(defmethod spit-gzipped! java.io.BufferedReader [path content]
+  (io/make-parents path)
+  (with-open [s (-> path
+                    clojure.java.io/output-stream
+                    java.util.zip.GZIPOutputStream.
+                    clojure.java.io/writer)]
+    (binding [*out* s]
+      (loop [ln (.readLine content)]
+        (when ln
+          (println ln)
+          (recur (.readLine content)))))))
+
+(defmethod spit-gzipped! java.lang.String [path content]
   (io/make-parents path)
   (with-open [s (-> path
                     clojure.java.io/output-stream
@@ -25,12 +40,22 @@
   (with-open [in (java.util.zip.GZIPInputStream. (io/input-stream path))]
     (slurp in)))
 
+(defn iri->upstream-path [iri]
+  (io/as-relative-path (str "tmp/" (.getPath (java.net.URL. iri)))))
+
 (defn fetch-upstream [iri]
-  (http/get
-   iri
-   (fn [{:keys [status headers body error]}]
-     (if (= status 200)
-       (let [version-iri (xml-string->version-iri body)
-             fname (io/as-relative-path (str "tmp/" (.getPath (java.net.URL. version-iri))))]
-         (spit-gzipped! fname body))
-       [:TODO "Log this somewhere" status error]))))
+  (if (re-find #"^ftp" iri)
+    (let [rdr (java.io.BufferedReader. (java.io.InputStreamReader. (.openStream (java.net.URL. iri))))
+          version-iri (xml->version-iri rdr)
+          fname (iri->upstream-path version-iri)]
+      (spit-gzipped! fname rdr))
+    (http/request
+     {:url iri
+      :follow-redirects false}
+     (fn [{:keys [status headers body error]}]
+       (case status
+         200 (let [version-iri (xml-string->version-iri body)
+                   fname (iri->upstream-path version-iri)]
+               (spit-gzipped! fname body))
+         (301 302 303 307 308) (fetch-upstream (:location headers))
+         (throw (Exception. (str "TODO: Handle status " status))))))))

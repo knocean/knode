b61044ae195d33c2d8be342d318ea22e3a968f50
inaimathi <leo.zovic@gmail.com>
2017-06-19 00:37:29 -0400

    Dummied response handler for editor button

--- src/knode/front_end/core.cljs
+++ src/knode/front_end/core.cljs
@@ -3,3 +3,15 @@
 (def editor (.edit js/ace "editor"))
 (.setTheme editor "ace/theme/monokai")
 (-> editor (.getSession) (.setMode "ace/mode/sqlserver"))
+
+(-> (js/$ ".send-query")
+    (.click
+     (fn []
+       (let [query (.getValue editor)]
+         ;; (-> editor)
+         (.log js/console query)
+         (-> js/$
+             (.get "/query" (clj->js {:sparql query}))
+             (.done (fn [data]
+                      (.log js/console "GOT RESPONSE")
+                      (.log js/console data))))))))

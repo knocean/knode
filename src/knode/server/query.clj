faf3faf5ac95f8570cce19decb89bd0d726537e2
inaimathi <leo.zovic@gmail.com>
2017-07-03 16:08:08 -0400

    First crack at inserting limit and offset

--- src/knode/server/query.clj
+++ src/knode/server/query.clj
@@ -1,7 +1,28 @@
 (ns knode.server.query
   (:require
+   [clojure.string :as string]
    [knode.server.template :as pg]
    [knode.server.util :as util]))
+
+(def +default-limit+ 200)
+
+(defn -ensure-limit [query]
+  (if-let [res (re-find #"LIMIT (\d+)" query)]
+    (let [given-limit (read-string (second res))]
+      (if (> given-limit +default-limit+)
+        [(string/replace query (re-pattern (first res)) (str "LIMIT " +default-limit+)) +default-limit+]
+        [query given-limit]))
+    [(str query "\nLIMIT " +default-limit+) +default-limit+]))
+
+(defn -ensure-offset [query limit page]
+  (if (or (= 0 page) (re-find #"OFFSET (\d+)" query))
+    query
+    (str query "\nOFFSET " (* limit page))))
+
+(defn sanitized-sparql
+  [query & {:keys [page] :or {page 0}}]
+  (let [[query limit] (-ensure-limit query)]
+    (-ensure-offset query limit page)))
 
 (defn render-query-interface
   [req]

72563cb87532ea2c5c5e0b73bdb9a61dc07d2ceb
inaimathi <leo.zovic@gmail.com>
2017-07-17 18:36:12 -0400

    Remove WIP line in server/server

--- src/knode/server/server.clj
+++ src/knode/server/server.clj
@@ -844,7 +844,6 @@
 
   (ANY "/api/query" [:as req] (query-request! state req))
   (GET "/query" [] query/render-query-interface)
-  (GET "/query/")
 
   ; doc directory
   (GET "/doc/:doc.html" [doc :as req] (render-doc req doc))

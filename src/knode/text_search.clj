c1cfa76b364485e9d481142ac65dec50a01ca0a3
inaimathi <leo.zovic@gmail.com>
2017-08-31 12:13:02 -0400

    Working back-end for text search interface

--- src/knode/text_search.clj
+++ src/knode/text_search.clj
@@ -1,13 +1,48 @@
 (ns knode.text-search
-  (:require [clucy.core :as clucy]))
+  (:require [clucy.core :as clucy]
+            [me.raynes.fs :as fs]
 
-(def +search-results+ 25)
-(def index (clucy/disk-index "./search-index"))
+            [knode.state :refer [state]]))
 
-(defn add! [map]
-  (clucy/add index map))
+(def +max-search-results+ 25)
+(def +search-index-dir+ "tmp/search-index")
+(def index (clucy/disk-index +search-index-dir+))
+
+(def rdf-label "http://www.w3.org/2000/01/rdf-schema#label")
+(def alternative-term "http://purl.obolibrary.org/obo/IAO_0000118")
+(def indexed-predicates
+  #{rdf-label alternative-term})
+
+
+(defn clear-index! []
+  (fs/delete-dir +search-index-dir+))
+
+(defn add! [map] (clucy/add index map))
+
+(defn add-term! [term]
+  (let [[iri subject-map] term
+        index-map (merge
+                   {:iri iri}
+                   (->> subject-map
+                        :blocks
+                        (filter #(contains? indexed-predicates (get-in % [:predicate :iri])))
+                        (map #(let [o (:object %)]
+                                [(keyword (get-in % [:predicate :label]))
+                                 (or (:lexical o) (:name o) (:label o) (:iri o))]))
+                        (into {})))]
+    (add! index-map)))
+
+(defn rebuild-index! []
+  (doseq [term (:terms @state)]
+    (add-term! term)))
 
 (defn search [term]
-  (clucy/search index term +search-results+))
+  (map
+   clojure.walk/stringify-keys
+   (try
+     (clucy/search index term +max-search-results+)
+     (catch org.apache.lucene.store.NoSuchDirectoryException e
+       []))))
 
-(defn complete [term] [])
+(defn complete [term]
+  [])

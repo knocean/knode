0561136ab9c89c13fc5095a8aeecb16a7980051a
inaimathi <leo.zovic@gmail.com>
2018-06-03 18:32:15 -0400

    Add honeysql

--- project.clj
+++ project.clj
@@ -20,6 +20,7 @@
                  [org.apache.jena/jena-iri "3.7.0"]
                  [org.apache.jena/jena-tdb "3.7.0"]
 
+                 [honeysql "0.9.2"]
                  [org.clojure/java.jdbc "0.7.6"]
                  [org.xerial/sqlite-jdbc "3.21.0"]
                  [org.postgresql/postgresql "42.2.2"]
--- src/com/knocean/knode/linked_data_fragments/sql.clj
+++ src/com/knocean/knode/linked_data_fragments/sql.clj
@@ -2,7 +2,7 @@
   (:require [clojure.edn :as edn]
             [clojure.string :as string]
             [clojure.set :as set]
-            [clojure.java.jdbc :as sql]
+            [clojure.java.jdbc :as jdbc]
 
             [org.knotation.util :as util]
             [org.knotation.rdf :as rdf]
@@ -17,18 +17,18 @@
 ;;    :subname "resources/obi_core.db"})
 ;; (defn dummy-db!
 ;;   [db]
-;;   (sql/with-db-connection [handle db]
-;;     (sql/execute! handle ["drop table if exists ontology"])
-;;     (sql/execute! handle [(sql/create-table-ddl
+;;   (jdbc/with-db-connection [handle db]
+;;     (jdbc/execute! handle ["drop table if exists ontology"])
+;;     (jdbc/execute! handle [(jdbc/create-table-ddl
 ;;                            :ontology
 ;;                            (map (fn [name] [name :string])
 ;;                                 [:gi :si :sb :pi :oi :ob :ol :di :ln]))])
-;;     (doseq [d (:maps @st/state)] (sql/insert! handle :ontology d))))
+;;     (doseq [d (:maps @st/state)] (jdbc/insert! handle :ontology d))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn -query->sql-where-clause
   [query]
-  (let [relevant (base/remove-falsies (select-keys query [:gi :si :pi :oi :ol :ln :di]))]
+  (let [relevant (base/remove-falsies (select-keys query [:gi :si :sb :pi :oi :ol :ln :di]))]
     (when (not (empty? relevant))
       (cons
        (str " WHERE " (string/join " AND " (remove nil? (map #(str (name %) "=?") (keys relevant)))))
@@ -57,7 +57,7 @@
         [where & params] (-query->sql-where-clause query)]
     (map
      #(into {} (filter second %))
-     (sql/query
+     (jdbc/query
       source
       (vec
        (cons
@@ -72,7 +72,7 @@
     (second
      (first
       (first
-       (sql/query
+       (jdbc/query
         db (vec
             (cons
              (string/join [base where pagination])
@@ -80,6 +80,6 @@
 
 (defmethod query :database ;; if we've got a database, we need to query it for matching entries
   [query data]
-  (sql/with-db-connection [db data]
+  (jdbc/with-db-connection [db data]
     {:total (count! query db) :per-page (:per-page query) :page (:page query)
-     :items (vec (map #(into {} (filter second %)) (sql/query db (query->sql query))))}))
+     :items (vec (map #(into {} (filter second %)) (jdbc/query db (query->sql query))))}))

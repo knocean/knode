d2cd6591108b111b1ce3048811d7fbade6268ca2
rctauber <rctauber@gmail.com>
2019-07-18 10:58:39 -0700

    Render anonymous superclasses in subject details

--- src/com/knocean/knode/pages/ontology/html.clj
+++ src/com/knocean/knode/pages/ontology/html.clj
@@ -4,6 +4,7 @@
 
             [org.knotation.rdf :as rdf]
             [org.knotation.environment :as en]
+            [org.knotation.omn :as omn]
 
             [com.knocean.knode.util :as util]
             [com.knocean.knode.state :refer [state] :as st]
@@ -30,6 +31,49 @@
      " "
      (en/iri->name env iri)]))
 
+(declare sort-statements)
+
+(defn render-statement
+  "Adapted from omn/render-statement."
+  [{:keys [::en/env ::st/exact ::rdf/quad] :as state}]
+  (cond
+    exact (->> exact flatten (filter string?))
+    :else
+    (when (and (::rdf/oi quad) (not= (rdf/rdf "nil") (::rdf/oi quad)))
+     (let [name (en/iri->name env (::rdf/oi quad))]
+       (if (re-find #"\s" name)
+         [:a {:href (::rdf/oi quad)} (str "'" name "'")]
+         [:a {:href (::rdf/oi quad)} name])))))
+
+(defn get-blank-states
+  [env bnode]
+  (let [trps (st/query {:select [:pi :oi :ob] :from [:states] :where [:= :sb bnode]})]
+    (reduce 
+      (fn [states trp]
+        (conj 
+          states 
+          {::en/env env
+           ::st/event ::st/statement
+           ::rdf/quad {::rdf/pi (:pi trp) 
+                       ::rdf/oi (:oi trp) 
+                       ::rdf/ob (:ob trp)} 
+           ::rdf/subject bnode}))
+      [] trps)))
+
+(defn render-omn
+  [env bnode]
+  (let [states (get-blank-states env bnode)]
+    (->> states
+         (group-by ::rdf/subject)
+         (#(assoc %
+                 ::rdf/subjects (->> states (map ::rdf/subject) distinct)
+                 ::st/states []))
+         sort-statements
+         ::st/states
+         (map render-statement)
+         (concat [:span])
+         (into []))))
+
 (defn render-pair
   [env {:keys [pi oi ob ol] :as state}]
   [:li
@@ -37,7 +81,7 @@
    ": "
    (cond
      oi [:a {:href oi :property (en/iri->curie env pi)} (en/iri->name env oi)]
-     ob ob
+     ob (render-omn env ob)
      ol [:span {:property (en/iri->curie env pi)} ol])])
 
 (def obo (partial apply str "http://purl.obolibrary.org/obo/"))
@@ -131,3 +175,150 @@
      {:session session
       :title (:project-name @state)
       :content (map render-subject-html requested-iris)})))
+
+;; -----------------------------------------
+;; OMN rendering helper function
+;; -----------------------------------------
+
+(defn sort-statements
+  "Adapted from omn/sort-statements."
+  [coll]
+  (loop [{:keys [::rdf/subjects ::depth] :as coll} coll]
+    (if-let [subject (first subjects)]
+      (recur
+       (if (first (get coll subject))
+         (let [coll (->> (get coll subject)
+                         (map #(assoc % ::omn/omn true))
+                         (assoc coll subject))
+               states (get coll subject)
+               state (first states)
+               rdf-type        (omn/find-state states (rdf/rdf "type"))
+               complement-of   (omn/find-state states (rdf/owl "complementOf"))
+               intersection-of (omn/find-state states (rdf/owl "intersectionOf"))
+               union-of        (omn/find-state states (rdf/owl "unionOf"))
+               on-property     (omn/find-state states (rdf/owl "onProperty"))
+               some-values     (omn/find-state states (rdf/owl "someValuesFrom"))
+               all-values      (omn/find-state states (rdf/owl "allValuesFrom"))
+               one-of          (omn/find-state states (rdf/owl "oneOf"))
+               first-item      (omn/find-state states (rdf/rdf "first"))
+               rest-item       (omn/find-state states (rdf/rdf "rest"))]
+           (cond
+             complement-of
+             (let [ob (-> complement-of ::rdf/quad ::rdf/ob)]
+               (-> coll
+                   (update ::st/states conj (assoc rdf-type ::st/silent true))
+                   (update ::st/states conj (assoc complement-of
+                                                   ::st/before
+                                                   (concat
+                                                    [[:keyword "not"]
+                                                     [:space " "]]
+                                                    (when ob [[:symbol "("]]))))
+                   (update subject (partial remove #{rdf-type complement-of}))
+                   (update ::depth (fnil inc 0))
+                   (assoc ::rdf/subjects (concat
+                                          (when ob [ob])
+                                          subjects))))
+
+             one-of
+             (let [ob (-> one-of ::rdf/quad ::rdf/ob)]
+               (-> coll
+                   (update ::st/states conj (assoc rdf-type ::st/silent true))
+                   (update ::st/states conj (assoc one-of ::st/before [[:symbol "{"]]))
+                   (update subject (partial remove #{rdf-type one-of}))
+                   (update ::depth (fnil inc 0))
+                   (assoc ::rdf/subjects (concat
+                                          (when ob [ob])
+                                          subjects))))
+
+             (or union-of intersection-of)
+             (-> coll
+                 (update ::st/states conj (assoc rdf-type ::st/silent true))
+                 (update ::st/states conj (assoc (or union-of intersection-of) ::st/silent true))
+                 (update subject (partial remove #{rdf-type union-of intersection-of}))
+                 (assoc ::rdf/subjects (concat
+                                        (when-let [ob (-> union-of ::rdf/quad ::rdf/ob)] [ob])
+                                        (when-let [ob (-> intersection-of ::rdf/quad ::rdf/ob)] [ob])
+                                        subjects)))
+
+             on-property
+             (let [obp (-> on-property ::rdf/quad ::rdf/ob)
+                   obv (-> (or some-values all-values) ::rdf/quad ::rdf/ob)
+                   keyword (concat
+                            (when obp [[:symbol ")"]])
+                            [[:space " "]
+                             [:keyword (if some-values "some" "only")]
+                             [:space " "]]
+                            (when obv [[:symbol "("]]))]
+               (-> coll
+                   (update ::st/states conj (assoc on-property ::st/before (when obp [[:symbol "("]])))
+                   (update ::st/states conj (assoc rdf-type ::st/exact keyword))
+                   (update subject (partial remove #{rdf-type on-property}))
+                   (assoc ::rdf/subjects (concat
+                                          (when obp [obp])
+                                          (when obv [obv])
+                                          subjects))))
+
+             (or some-values all-values)
+             (let [obv (-> (or some-values all-values) ::rdf/quad ::rdf/ob)]
+               (-> coll
+                   (update ::st/states conj (assoc (or some-values all-values) ::st/after (when obv [[:symbol ")"]])))
+                   (update subject (partial remove #{some-values all-values}))))
+
+             first-item
+             (if-let [ob (-> first-item ::rdf/quad ::rdf/ob)]
+               (-> coll
+                   (update ::st/states conj (assoc first-item ::st/silent true))
+                   (update subject (partial remove #{first-item}))
+                   (assoc ::rdf/subjects (concat [ob] subjects)))
+               (-> coll
+                   (update ::st/states conj first-item)
+                   (update subject (partial remove #{first-item}))))
+
+             rest-item
+             (let [collection
+                   (->> coll
+                        ::st/states
+                        reverse
+                        (map ::rdf/quad)
+                        (map ::rdf/pi)
+                        (filter #{(rdf/owl "unionOf") (rdf/owl "intersectionOf") (rdf/owl "oneOf")})
+                        first)]
+               (if-let [ob (-> rest-item ::rdf/quad ::rdf/ob)]
+                 (-> coll
+                     (update ::st/states conj (assoc rest-item
+                                                     ::st/exact
+                                                     (condp = collection
+                                                       (rdf/owl "unionOf") [[:space " "] [:keyword "or"] [:space " "]]
+                                                       (rdf/owl "intersectionOf") [[:space " "] [:keyword "and"] [:space " "]]
+                                                       (rdf/owl "oneOf") [[:symbol ","] [:space " "]])))
+                     (update subject (partial remove #{rest-item}))
+                     (assoc ::rdf/subjects (concat [ob] subjects)))
+                 (if (and depth (> depth 0))
+                   (-> coll
+                       (update ::st/states conj (assoc rest-item ::st/exact [(if (= collection (rdf/owl "oneOf")) [:symbol "}"] [:symbol ")"])]))
+                       (update subject (partial remove #{rest-item}))
+                       (update ::depth dec))
+                   (-> coll
+                       (update ::st/states conj (assoc rest-item ::st/silent true))
+                       (update subject (partial remove #{rest-item}))))))
+
+             ; this should never be reached?
+             :else
+             (-> coll
+                 (update ::st/states conj state)
+                 (update subject rest))))
+         ; no more states for this subject
+         (-> coll
+             (dissoc subject)
+             (update ::rdf/subjects rest))))
+      ; no more subjects
+      (let [states (::st/states coll)
+            last-state (last states)]
+        (assoc
+         coll
+         ::st/states
+         (conj (vec (butlast states))
+               (-> last-state
+                   (dissoc ::st/silent)
+                   (assoc ::last true))))))))
+
